47737b0c2c5abb7af3ac7285febdea6faa3aade4
eungyeol <leepro225@gmail.com>
2021-08-10 23:32:00 +0900

    getter setter

--- 3-OOP/3-3-encapsulation.ts
+++ 3-OOP/3-3-encapsulation.ts
@@ -7,14 +7,10 @@
     // private : 외부에서 접근 불가능한 상태로 만듦
     // protected : 외부에서는 접근 불가능, 이 클래스를 상속한 자식에서는 접근 가능
     class CoffeeMaker {
-        private coffeeBeans: number = 0; // instance (object) level
-        // 멤버 변수를 작성할때는 let, const , function 필요 없음
-        private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
-        // 클래스에서 정해졌으며 변하지 않는 것은 static을 붙여준다.
-        // 그럼 인스턴스를 생성할때마다 중복적으로 데이터를 생성하지 않아 메모리를 절약할 수 있다.
+        private coffeeBeans: number = 0; 
+        private static BEANS_GRAMM_PER_SHOT: number = 7;
 
         private constructor(coffeeBeans: number) {
-            // 인스턴스를 만들때 항상 호출되는 함수
             this.coffeeBeans = coffeeBeans;
         }
 
@@ -33,7 +29,6 @@
         }
         
         makeCoffee(shots: number): CoffeeCup {
-            // 클래스 안에 있는 친구에 접근할때는 this를 붙여준다.
             if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
                 throw new Error('Not enough coffee beans!')
             }
@@ -50,12 +45,71 @@
     // const maker = new CoffeeMaker(21);
     // 이런 식으로 호출하도록 유도
     const maker = CoffeeMaker.makeMachine(34);
-    
+
     // maker.coffeeBeans = -34 이처럼 외부에서 잘못된 값을 입력해 상태를 잘못 변경할 수 있음
     // private 를 붙이면, 접근하지 못함
     // 이렇게 하면, 외부에서 값을 바로 변경하는 게 아니라, 함수에서 한번 걸러서 변경할 수있음
     maker.fillCoffeeBeans(5); 
 
-    
+   class User {
+       firstName: string;
+       lastName: string;
+       fullName: string;
+       constructor(firstName: string, lastName: string) {
+           this.firstName = firstName;
+           this.lastName = lastName;
+           this.fullName = `${firstName} ${lastName}`;
+       }
+   }
+   const user = new User('Steve', 'Jobs');
+   console.log(user.fullName); // Steve Jobs
+   user.firstName = 'Ellie';
+   console.log(user.fullName); // Steve Jobs, 
+   //fullName이 설정된 뒤로 firstName이나 lastName이 변경되어도
+   // 다시 fullName이 계산되지 않고, 한번 할당되면 계속 지정되어 있음
+   // 이럴경우 getter가 유용하게 쓰임
 
+   class User2 {
+        firstName: string;
+        lastName: string;
+        // 멤버 변수를 바로 선언하고 설정하는 것이 아니라, get을 
+        // 이용해서 fullName을 정의, fullName에 접근할 때마다
+        // 호출 시점에 새로운 데이터를 만들고, 계산 
+       get fullName(): string {
+           return `${this.firstName} ${this.lastName}`
+       }
+       constructor(firstName:string, lastName: string) {
+           this.firstName = firstName;
+           this.lastName = lastName;
+       }
+       // 이렇게 간단하게도 할 수 있다.
+        //    constructor(private firstName:string, public lastName: string) {
+        // }
+   }
+   const user2 = new User2('Steve', 'Jobs');
+   console.log(user2.fullName); // Steve Jobs
+   user2.firstName = 'Ellie';
+   // 함수처럼 접근하는 게 아니고, 변수처럼 접근하면 됨.
+   console.log(user2.fullName); // Ellie Jobs, 
+
+   // getter 와 setter
+   class User3 {
+       private internalAge = 4;
+       get age(): number {
+           return this.internalAge
+       }
+       set age(num: number) {
+           if (num < 0) {
+               // 정확한 숫자인지 유효성 검사를 할 수 있고
+               throw new Error('too young!');
+           }
+           // 바로 설정하는 게 아닌, 더 연산을 여기서 할 수 도 있음
+           // private 인 internalAge 를 업데이트 할 수 있음
+           this.internalAge = num;
+       }
+       constructor(private firstName: string, public lastName:string) {}
+    }
+
+    const user3 = new User3('Steve', 'Jobs');
+    user3.age = 6;
 }
